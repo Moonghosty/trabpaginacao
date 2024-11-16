20d7a05e3737d9cf3189a126ffbb204a3658a583
Moonghosty <128601188+Moonghosty@users.noreply.github.com>
2024-11-16 02:35:12 -0300

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,13 @@
+itens = [
+    {"id": 1, "nome": "Abacate", "descricao": "Verde e nutritivo"},
+    {"id": 2, "nome": "Banana", "descricao": "Amarelas e ricas em potássio"},
+    {"id": 3, "nome": "Acerola", "descricao": "Vermelhas e laranjas, fonte de vitamina C"},
+    {"id": 4, "nome": "Uva", "descricao": "Verdes ou roxas, ricas na vitamina B"},
+    {"id": 5, "nome": "Melancia", "descricao": "Vermelha por dentro, rica em vitamina A"},
+    {"id": 6, "nome": "Laranja", "descricao": "Fruta cítrica e alaranjada, rica em vitamina C"},
+    {"id": 7, "nome": "Manga", "descricao": "Laranja vibrante, rica em vitamina A"},
+    {"id": 8, "nome": "Morango", "descricao": "Vermelho e rico em vitamina C"},
+    {"id": 9, "nome": "Maçã", "descricao": "Vermelha ou verde, fonte de antioxidantes"},
+    {"id": 10, "nome": "Pera", "descricao": "Verde ou amarelada, fonte de fibras e vitamina K"},
+]
+# Aqui está guardada a lista de itens que vão aparecer no html
